854db6e5519364d7b31e2db5921b1b17817cc3a0
Siva-ra <sivaram@effeindia.com>
2026-07-06 17:40:20 +0530

    add cores

--- server.js
+++ server.js
@@ -11,6 +11,14 @@ require("dotenv").config();
 
 const app = express();
 
+app.use(cors({
+    origin: [
+        "https://lightgreen-cheetah-775075.hostingersite.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
 /* ===== MIDDLEWARE ===== */
 app.use(cors());
 app.use(express.json());
