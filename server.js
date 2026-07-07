826fb5ec0017c825aef0da55f863147f3ba8bd95
Siva-ra <sivaram@effeindia.com>
2026-07-07 17:39:12 +0530

    add  cors

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ require("dotenv").config();
 const app = express();
 
 /* ===== MIDDLEWARE ===== */
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
