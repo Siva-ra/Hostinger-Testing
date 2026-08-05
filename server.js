c28c2621ed30515753d7d7e089b816394d8132cc
Siva-ra <sivaram@effeindia.com>
2026-08-05 09:48:57 +0530

    image code has been removed

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ if (!fs.existsSync(uploadFolder)) {
     console.log("Upload folder already exists");
 }
 
+//Uploads Folder
 app.use(
     "/uploads",
     (req, res, next) => {
@@ -89,6 +90,17 @@ app.use(
     },
     express.static(uploadFolder)
 );
+
+//Photos Folder
+app.get("/photo/:filename", (req, res) => {
+    const file = path.join(photoFolder, req.params.filename);
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+
+    res.sendFile(file);
+});
 
 /* ===== DATABASE ===== */
 const db = mysql.createPool({
